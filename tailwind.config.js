909b7ee0ab75efaf0fd95383ef10a49b0b213253
matlavv <mathis.laversin92@gmail.com>
2024-05-02 09:24:52 +0200

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  colors: {
+    "primary-purple": "#FFD6FF",
+    "secondary-purple": "#E7C6FF",
+    "third-purple": "#E7C6FF",
+    "primary-blue": "#B8C0FF",
+    "secondary-blue": "#BBD0FF",
+  },
+  plugins: [],
+};
